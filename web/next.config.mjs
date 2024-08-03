1666a6d011e19fbd81f3e067827adc98fd507c62
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-08-03 13:31:28 +0545

    feat: docker configs

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,7 +1,7 @@
 const API_HOST =
   process.env.NODE_ENV === "development"
     ? "http://127.0.0.1:8000"
-    : "http://backend:8000";
+    : "http://api:8000";
 
 const nextConfig = {
   trailingSlash: true,
