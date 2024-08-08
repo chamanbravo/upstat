6f49ec0eaf717a085f21f2d7d2f9653acc64752f
callback cat <76839908+chamanbravo@users.noreply.github.com>
2024-08-08 15:05:22 +0545

    fix: default cache

--- web/app/auth/page.tsx
+++ web/app/auth/page.tsx
@@ -23,11 +23,6 @@ export default async function index() {
   return (
     <div className="mt-4 px-4 flex justify-center items-center  md:h-[100vh] md:mt-0 md:px-0">
       {needSetup ? <RegisterForm /> : <LoginForm />}
-      {needSetup === undefined ? null : needSetup ? (
-        <RegisterForm />
-      ) : (
-        <LoginForm />
-      )}
 
       <span className="absolute p-2 w-[90%] sm:w-fit text-muted-foreground border text-sm rounded bottom-10 bg-card">
         This is a demo of upstat dashboard. The database resets periodically.
