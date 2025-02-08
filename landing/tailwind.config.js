3c0f783757bcbbc14afbb43d86a4adc9e4e3e15d
callback cat <76839908+chamanbravo@users.noreply.github.com>
2025-02-08 20:51:45 +0545

    feat: homepage

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.{html,js}"],
-    theme: {
-        extend: {},
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      borderImage: {
+        "gradient-radial":
+          "radial-gradient(rgba(114, 125, 161, 0.20), transparent)",
+      },
     },
-    plugins: [],
-}
+  },
+  plugins: [],
+};
