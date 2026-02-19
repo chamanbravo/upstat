2bfde50d2ab93ae297199c1abb44229c30e932f0
ktyborowski <kamilty10@gmail.com>
2026-02-19 12:03:15 +0100

    Set pragmas via query

--- apps/api/database/sqlite.go
+++ apps/api/database/sqlite.go
@@ -7,7 +7,18 @@ import (
 )
 
 func SqliteConnection() (*sql.DB, error) {
-	sqliteDSN := "file:upstat.db?_pragma=journal_mode(WAL)&_pragma=synchronous(NORMAL)&_pragma=cache_size(-64000)&_pragma=temp_store(MEMORY)"
+	sqliteDSN := "upstat.db"
 	db, err := sql.Open("sqlite3", sqliteDSN)
+
+	ddl := "PRAGMA journal_mode = WAL;" +
+		"PRAGMA synchronous = NORMAL;" +
+		"PRAGMA cache_size = 2000;" +
+		"PRAGMA mmap_size = 134217728;" +
+		"PRAGMA journal_size_limit = 27103364;" +
+		"PRAGMA temp_store = MEMORY;" +
+		"PRAGMA foreign_keys = ON;"
+
+	_, err = db.Exec(ddl)
+
 	return db, err
 }
